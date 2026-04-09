30e80433b3e9bc9be21bd1b2782e0c2bcffd18ef
Nicholas Cardona <nicholascardona7@gmail.com>
2026-04-09 20:51:02 +0200

    Remove broken config for lsp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -3,13 +3,4 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "docker" }
 vim.lsp.enable(servers)
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
 vim.lsp.config("roslyn", {})
--- read :h vim.lsp.config for changing options of lsp servers 
